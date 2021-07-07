cdd4deeb4f3afc3493b7714b91e4df16eaf1d115
Sruthi1045 <84717209+Sruthi1045@users.noreply.github.com>
2021-07-07 19:49:56 +0530

    my solution

--- phone_book.c
+++ phone_book.c
@@ -62,8 +62,28 @@ int main(int argc, char *argv[]) {
     fclose(fp);
     exit(0);
   } else if (strcmp(argv[1], "search") == 0) {  /* Handle search */
-    printf("NOT IMPLEMENTED!\n"); /* TBD  */
-  } else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
+    FILE *fp = open_db_file();
+    char *name=argv[2];
+    entry *p=load_entries(fp);
+    entry *base=p;
+    while(p!=NULL){
+      if(strcmp(name,p->name) == 0){
+        printf("%s",p->phone);
+        break;
+      }
+      p=p->next; 
+    }
+    if(p==NULL){
+      printf("no match");
+      free_entries(base);
+      fclose(fp);
+      exit(1);
+    }
+    free_entries(base);
+    fclose(fp);
+    exit(0);  
+  } 
+    else if (strcmp(argv[1], "delete") == 0) {  /* Handle delete */
     if (argc != 3) {
       print_usage("Improper arguments for delete", argv[0]);
       exit(1);
@@ -94,7 +114,13 @@ FILE *open_db_file() {
   
 void free_entries(entry *p) {
   /* TBD */
-  printf("Memory is not being freed. This needs to be fixed!\n");  
+  entry *q;
+  q=p;
+  while(p){
+    q=p;
+    p=p->next;
+    free(q);
+  }
 }
 
 void print_usage(char *message, char *progname) {
@@ -178,11 +204,14 @@ void add(char *name, char *phone) {
 void list(FILE *db_file) {
   entry *p = load_entries(db_file);
   entry *base = p;
+  int count0;
   while (p!=NULL) {
     printf("%-20s : %10s\n", p->name, p->phone);
+    count++;
     p=p->next;
   }
   /* TBD print total count */
+  printf("Total entries: %d",count);
   free_entries(base);
 }
 
@@ -191,7 +220,7 @@ int delete(FILE *db_file, char *name) {
   entry *p = load_entries(db_file);
   entry *base = p;
   entry *prev = NULL;
-  entry *del = NULL ; /* Node to be deleted */
+  entry del = NULL ; /* Node to be deleted */
   int deleted = 0;
   while (p!=NULL) {
     if (strcmp(p->name, name) == 0) {
@@ -207,7 +236,25 @@ int delete(FILE *db_file, char *name) {
       */
 
       /* TBD */
+      if(p==base){
+        del=p;
+        p=p->next;
+        base=p;
+        deleted++;
+        free(del);
+      }else{
+        del=p;
+        prev->next=p->next;
+        p=p->next;
+        deleted++;
+        free(del);
+      }
+     
+    }else{
+       prev=p;
+       p=p->next;
     }
+    
   }
   write_all_entries(base);
   free_entries(base);
